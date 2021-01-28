2b620b6af7bbd9b1fe5e34c4ac1874f0e3d56d4f
naoki.kubota <nk.0110.6@gmail.com>
2021-01-28 12:28:38 +0000

    omikuzi game

--- omikuzi/omikuzi.js
+++ omikuzi/omikuzi.js
@@ -1,9 +1,10 @@
 `use strict`
-
 {
   const btn = document.getElementById('btn');
   
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!';
+    const results = ["大吉","中吉","凶", "末吉"];
+    btn.textContent = results[Math.floor(Math.random() * results.length)];
   });
 }
+  
