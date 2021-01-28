c86bf9cca654ab3a967554da0d622442ce376c73
naoki.kubota <nk.0110.6@gmail.com>
2021-01-28 08:09:36 +0000

    init f

---
+++ index.js
@@ -0,0 +1,41 @@
+// let unko = "hello world";
+
+// // unko = "helo world2";e
+
+// const bigUnko = "hello javascript";
+
+// console.log(bigUnko);
+
+const inoki = [`ichi`,`ni`,`san`,`da-----!!!`];
+// // console.log(inoki[3]);
+
+// let index = 0;
+
+// while(index < inoki.length) {
+//   console.log(inoki[index]);
+//   index++;
+// }
+
+// if else
+
+
+const test = (arg) => {
+  if(inoki.length > arg) {
+  console.log("bonbaie");
+  }else {
+  console.log("b");
+  }
+};
+
+const unko2 = {
+  color: "pink",
+  size: "large",
+  purfume: "mint",
+  go: () => {
+    console.log(`hello`);
+  }
+};
+
+document.getElementsByTagName(`button`)[0].addEventListener(`click`, ()=> {
+  window.alert("hello");
+});
