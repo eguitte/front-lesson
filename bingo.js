f09703d25e33166e98b086918ebf7310972a36ed
naoki.kubota <nk.0110.6@gmail.com>
2021-01-29 13:38:36 +0000

    bingo line1

---
+++ bingo.js
@@ -0,0 +1,11 @@
+const source = [];
+for (let i = 0; i < 15; i++) {
+  source[i] = i + 1;
+}
+
+const b = [];
+for (let i = 0; i < 5; i++) {
+  b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+}
+
+console.log(b);
