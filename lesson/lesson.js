c9df3f844c5919092a6b8db1aeb8b3620b3df6f1
naoki.kubota <nk.0110.6@gmail.com>
2021-01-31 08:50:05 +0000

    init lesson

--- lesson/lesson.js
+++ lesson/lesson.js
@@ -1,3 +1,9 @@
-'use strict';{
-  console.log('Hello World!');
-}
+'use strict';
+  let price = 150;
+  
+  console.log(price * 160);
+  console.log(price * 140);
+  
+  price = 980
+  console.log(price * 160);
+  console.log(price * 140);
