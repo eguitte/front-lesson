249f9e3aef03b099173b9d1af332b41bce9a8303
naoki.kubota <nk.0110.6@gmail.com>
2021-01-31 11:06:28 +0000

    finish lesson

--- lesson/lesson.js
+++ lesson/lesson.js
@@ -1,9 +1,83 @@
 'use strict';
-  let price = 150;
+
+  // const signal = "green";
   
-  console.log(price * 160);
-  console.log(price * 140);
+  // switch (signal) {
+  //   case "red":
+  //     console.log("stop!");
+  //     break;
+  //   case "yellow":
+  //     console.log("caution!");
+  //     break;
+  //   case "blue":
+  //   case "green":
+  //     console.log("go!");
+  //     break;
+  //   default:
+  //     console.log("wrong signal");
+  //     break;
+  // }
   
-  price = 980
-  console.log(price * 160);
-  console.log(price * 140);
+  // for (let i = 1; i <= 10; i++){
+  //   console.log(`hello ${i}`);
+  // }
+  
+  let hp = -50;
+  
+  while (hp > 0){
+    console.log(`${hp} HP`);
+    hp -= 15;
+  }
+  
+  for(let i = 1; i<= 10; i++){
+    if (i % 3 === 0){
+      // スキップするとき
+      // continue;
+    // 処理を終了させるとき
+      break;
+    }
+    console.log(i);
+  }
+  
+  // 関数で処理をまとめる
+  function showAd(message = "Ad") {
+    console.log('----------');
+    console.log(`${message}`);
+    console.log('----------');
+  }
+  
+  showAd("header Ad");
+    console.log('Tom is great!');
+    console.log('Bob is great!');
+  showAd();
+    console.log('Steve is great!');
+    console.log('Richard is great!');
+  showAd("footer Ad");
+  
+//   function sum(a, b, c) {
+//   // console.log(a + b + c);
+//   return a + b + c;
+// }
+
+//   // sum(1, 2, 3);
+//   // sum(3, 4, 5);
+  
+//   const total = sum(1, 2, 3) + sum(3, 4, 5); // 18
+//   console.log(total);
+  
+  // const sum = function(a, b, c) {
+  //   return a + b + c;
+  // };
+  
+  // const total = sum(1, 2, 3) + sum(3, 4, 5);
+  // console.log(total);
+  
+  // const sum = (a, b, c) => a + b + c;
+  // const total = sum(1, 2, 3) + sum(3, 4, 5);
+  // console.log(total);
+  
+  function f() {
+    const x = 1;
+    console.log(x);
+  }
+  f();
